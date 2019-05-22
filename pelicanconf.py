d53c99d987d9f357ae9c066eb7d6d70200243a36
fsarigoz <fsarigoz@gmail.com>
2019-05-21 19:28:24 -0700

    Initial Upload

---
+++ pelicanconf.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Fatih Sarigoz'
+SITENAME = 'Fatih Sarigoz'
+SITESUBTITLE = u'Data Science Blog'
+SITEURL = '' # Intentionally left blank, see ./publishconf.py
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+GITHUB_URL = 'http://github.com/fsarigoz'
+TWITTER_USERNAME = '@fatihsarigoz'
+
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/fatihsarigoz'),
+          ('linkedin', 'http://www.linkedin.com/in/fatihsarigoz'),
+          ('github', 'https://github.com/fsarigoz'),)
+
+DEFAULT_PAGINATION = 10
+SUMMARY_MAX_LENGTH = 50
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup', 'pelican-bootstrapify']
+
+BOOTSTRAPIFY = {
+    'table': ['table', 'table-striped', 'table-hover'],
+    'img': ['img-fluid'],
+    'blockquote': ['blockquote'],
+}
+
+# Theme settings --------------------------------------------------------------
+THEME = u'notmyidea'
+
+MAIN_MENU = True
+MENUITEMS = (('Archives', '/archives.html'),)
+
